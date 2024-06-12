ae8691438bcc4e60b8176ce5aeb340ed3a46f160
David Rolf <leepicrockyc@gmail.com>
2024-06-11 23:12:34 -0400

    fuck it we ball, (chunk gizmo don't work)

--- addons/world_streaming/chunk_gizmo_plugin.gd
+++ addons/world_streaming/chunk_gizmo_plugin.gd
@@ -3,45 +3,25 @@ extends EditorNode3DGizmoPlugin
 
 func _init():
 	set_name("ChunkGizmoPlugin")
+	create_material("main", Color(1, 0, 0))
+	create_handle_material("handles")
 
-func has_gizmo(spatial):
-	return spatial is WorldStreamer
+func has_gizmo(gizmo):
+	return gizmo is WorldStreamer
 
-func create_gizmo(spatial):
-	var gizmo = EditorNode3DGizmo.new()
-	gizmo.set_spatial_node(spatial)
+func _redraw(gizmo : EditorNode3DGizmo):
+	gizmo.clear()
+	var scene = gizmo.get_node_3d()
 
-	var chunk_manager = spatial as WorldStreamer
-	for scene in chunk_manager.chunk_scenes:
-		if scene:
-			var instance = scene.instance()
-			if instance:
-				instance.set_visible_in_tree(false)
-				var parent_transform = instance.global_transform
-				var mesh_instances = find_mesh_instances(instance)
-				for mesh_instance in mesh_instances:
-					var mesh = mesh_instance.mesh
-					if mesh:
-						var material = StandardMaterial3D.new()
-						material.albedo_color = Color(1, 1, 1, 0.5) # Grayed-out color
-						var mesh_gizmo = BoxMesh.new()
-						mesh_gizmo.set_mesh(mesh)
-						mesh_gizmo.set_material(material)
-						mesh_gizmo.set_parent(gizmo)
-						mesh_gizmo.set_global_transform(parent_transform * mesh_instance.global_transform)
-				
-				instance.queue_free()
+	var lines = PackedVector3Array()
 
-	return gizmo
+	lines.push_back(Vector3(0, 1, 0))
+	lines.push_back(Vector3(0, scene.global_transform.origin, 0))
 
-func find_mesh_instances(node):
-	var mesh_instances = []
+	var handles = PackedVector3Array()
 
-	if node is CSGBox3D:
-		mesh_instances.append(node)
+	handles.push_back(Vector3(0, 1, 0))
+	handles.push_back(Vector3(0, scene.global_transform.origin, 0))
 
-	for child in node.get_children():
-		var child_mesh_instances = find_mesh_instances(child)
-		mesh_instances.extend(child_mesh_instances)
-
-	return mesh_instances
+	gizmo.add_lines(lines, get_material("main", gizmo), false)
+	gizmo.add_handles(handles, get_material("handles", gizmo), [])
